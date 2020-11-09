ab90f97915df105991cda63e8dc1063d1855e611
Andrew Majors <adajors00@gmail.com>
2020-11-09 14:27:51 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ BangDiceGame/src/models/Dice.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Troll
+ */
+public class Dice {
+    private Die[] dice;
+    private final int numDie;
+    
+    public Dice(int numDie){
+        this.numDie = numDie;
+        rollDice();
+    }
+    
+    public void rollDice(){
+        for(int i=0; i<numDie; i++){
+            dice[i] = new Die(); 
+        }
+    }
+    public void rerollAllDice(){
+        for(int i=0; i<numDie; i++){
+            dice[i].roll();
+        }
+    }
+    public void rerollDice(Integer a, Integer b, Integer c, Integer d, Integer e, Integer f){
+        //you can specify up too 6 dice to re roll, all arguments are optional
+        //I thought this might be useful to say I want to reroll dice 1 2 and 4
+        //it can also be used to just reroll single dice ina for loop 
+        Integer[] indecies = {a,b,c,d,e,f};
+        for(int i=0; i<6; i++){
+            if(indecies[i] != null && indecies[i] < numDie){
+                dice[indecies[i]].roll();
+            }
+        }
+    }
+    
+    public Die[] getDice(){
+        return dice;
+    }
+}
---
+++ BangDiceGame/src/models/Die.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package models;
+import java.lang.Math; 
+/**
+ *
+ * @author Troll
+ */
+//instantiatting the die class creates an object with a random Sides value
+public class Die {
+    public enum Sides{arrow,
+                      dynamite,
+                      gatling,
+                      one_shot,
+                      two_shot,
+                      beer}; 
+    //To use the Sides enum in other classes ues Die.Sides.arrow or whichever
+    //you need.
+    
+    public Sides[] sidesList = Sides.values();
+    //sidesList is created so we can access the sides enum by index Sides.values
+    //retunrs the values of the enum as an array. This is used in the roll function
+
+    private Sides side;  
+    
+              
+    public Die(){
+       roll(); 
+    }
+    
+    public void roll(){
+        this.side = sidesList[(int)(Math.random() * 5)];
+        //math .random retunrs a value between 0 and 1 and we multiply by 5 to
+        //get a value between 0 and 5 bc we have 6 sides
+        
+    }
+    public Sides getSide(){
+        return side;
+    }
+}
