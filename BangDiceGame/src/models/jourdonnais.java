3c4909b542954f6f1c35b667039fa57fc40f03f4
Darrel Jackson <darrel.jackson@ttu.edu>
2020-11-18 08:55:01 -0600

    Jourdonnais
    class for the ability is done

--- BangDiceGame/src/models/jourdonnais.java
+++ BangDiceGame/src/models/jourdonnais.java
@@ -13,4 +13,15 @@ public class jourdonnais extends Character{
         public jourdonnais(Roles thisRole) {
         super(7, thisRole);
     }
+        
+        public void indianAttack() {
+            if(this.arrows >= 1){
+                this.bullets = this.bullets - 1; //Jourdonnais only lose 1 bullet due to his ablity.
+                this.arrows = 0;
+            }
+            else{
+                 this.bullets = this.bullets - 0; //Jourdonnais would not lose any health for indian attack.
+                 this.arrows = 0;
+            } 
+    }
 }
